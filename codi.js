6df2f13d2e659b69a0aabec2aaa8717ed24a8fa3
ejtorcello <50548234+ejtorcello@users.noreply.github.com>
2024-02-24 23:16:08 -0300

    Update codi.js
    funcionando con may

--- codi.js
+++ codi.js
@@ -2,12 +2,47 @@
 
 
 addEventListener('DOMContentLoaded', (event) => {
-    function almacenar(){sal.value = ent.value}
-    
     var sal = document.getElementById("salida");
-    var ent = document.getElementById("ingreso");
-    sal.value="todo"
-    ent.value="nada"
+var ent = document.getElementById("ingreso");
+var cadena=''
+
+    function almacenar(){
+        cadena=ent.value;
+        console.log('almacenar()  cadena= ',cadena);
+    codificar();
+        sal.value= cadena;
+
+    
+    }
+    let letra=''
+    function codificar(letra){
+        
+        console.log('codificar(), cadena= ',cadena)
+        
+        
+        //funcion anidada para insertar caracteres
+        function insertar(letra,i){console.log('insertar() letra= ',letra);
+                
+        
+            cadena = cadena.slice(0,i)+letra+cadena.slice(i+1);
+        
+
+        }
+        
+        for (let i=0; i<cadena.length; i++){
+            console.log('valor i= ', i)
+            letra = cadena[i]
+            console.log('letra= ',letra)
+            switch (letra){
+                case 'a': insertar('AAA',i)
+            break}
+           
+
+        }
+        
+        
+    }
+
 
 botonCodificar = document.getElementById("codificar")
 botonCodificar.addEventListener("click", almacenar)
